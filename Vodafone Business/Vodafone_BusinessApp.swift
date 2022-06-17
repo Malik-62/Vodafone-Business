783cf349f442377e71e293b2c3c9b7fc808c92fe
Malik-62 <67704241+Malik-62@users.noreply.github.com>
2022-06-17 18:01:41 +0200

    Initial Commit

---
+++ Vodafone Business/Vodafone_BusinessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vodafone_BusinessApp.swift
+//  Vodafone Business
+//
+//  Created by Malik Amajjoud on 17.06.22.
+//
+
+import SwiftUI
+
+@main
+struct Vodafone_BusinessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
