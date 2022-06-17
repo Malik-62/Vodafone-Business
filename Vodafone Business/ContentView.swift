b984a5abdf5b20fdbba786cb58e087c2a547df0a
Malik <amajjoud.malik@gmail.com>
2022-06-17 23:08:59 +0200

    rough sketch, muss noch in files unterteilt werden

--- Vodafone Business/ContentView.swift
+++ Vodafone Business/ContentView.swift
@@ -6,11 +6,32 @@
 //
 
 import SwiftUI
-
+let strNumber = "+491722029770"
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+        ScrollView {
+            Image("IM1").resizable().scaledToFit().cornerRadius(8).padding()
+            Image("IM2").resizable().scaledToFit().cornerRadius(5).padding()
+            Image("IM3").resizable().scaledToFit().cornerRadius(6).padding()
+            Image("IM4").resizable().scaledToFit().cornerRadius(7).padding()
+        }
+        
+        .shadow(radius: 20)
+        .navigationBarItems(leading: Image("Logo")
+            .resizable().padding(.all, 7.0).scaledToFit().position(x: 20, y: 30),
+            trailing:
+                                Button(action: {
+            let tel = "tel://"
+                let formattedString = tel + strNumber
+                guard let url = URL(string: formattedString) else { return }
+                UIApplication.shared.open(url)
+        }) {
+            Image(systemName: "phone.fill").foregroundColor(Color("VodaRot"))
+        })
+        .background(Image("Background").resizable())
+        }.background(Image("Background"))
+
     }
 }
 
